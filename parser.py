37b3414b1c88b9fb8afdd1f708a5cb6c1c7475ae
Daniel Teal <daniel@teals.org>
2016-10-23 05:22:34 -0500

    Parsed transcripts nicely.

--- parser.py
+++ parser.py
@@ -1,9 +1,8 @@
 import os
 def transcriptParser(fileName):
-	transcript = open(fileName, 'r')
-	
-	wordsToParse = transcript.read()
-	
+	with open(fileName, 'r') as transcript:
+		wordsToParse = transcript.read()
+
 	delete = ['[',']','{','}','.','?','!','"',',', ':', '\\', ' -', '(', ')']
 	wordsToParse = wordsToParse.replace('\\n', ' ')
 	for s in delete:
@@ -11,20 +10,11 @@ def transcriptParser(fileName):
 	wordsToParse = wordsToParse.replace('-', ' ')
 	wordsToParse = wordsToParse.lower()
 
-	
-	#print(wordsToParse)
-	
-	transcript.close()
-	path = ".\\new_transcription"
-	if not os.path.exists(path):
-		os.makedirs(path)
-	os.chdir(path)
-	output = open('new_' + fileName, 'w')
-	
-	output.write(wordsToParse)
-	output.close()
-	os.chdir("..")
-	
+	with open('processed' + fileName[8:-15] + '.txt', 'w') as output:
+		output.write(wordsToParse)
+
 for i in range(1, 1750):
-	if os.path.isfile(str(i) + '_transcript.txt'):
-		transcriptParser(str(i) + '_transcript.txt')
+	filename = 'original/' + str(i) + '_transcript.txt'
+	if os.path.isfile(filename):
+		transcriptParser(filename)
+
